2a20763a74f0a20deba5b3f0e5a737446489cf6d
Julián Campo <campocaldonoe@gmail.com>
2024-10-03 14:49:46 -0500

    Se crea el proyecto con el archivo main y una funcion con el hoola mundo

---
+++ funcionesProject/Main.py
@@ -0,0 +1,16 @@
+# Definicion de una funcion
+
+def my_function():
+    print("Hello from a function")
+
+#Invocacar la funcion
+my_function()
+
+def mostrarNombre(nombre):
+    print("Su nombre es "+nombre)
+
+mostrarNombre("Luis Vejarano")
+
+#Para ingresar por teclado
+nom = input("Ingrese su nombre: ")
+mostrarNombre(nom)
